4c82190fdfd7ed3e52e5c06042542c942b80259f
GitforZm <43491330+GitforZm@users.noreply.github.com>
2024-05-14 23:59:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ def run_detection():
     # 调用rectangle_detection.py中的函数
   
       
-    # 截取屏幕左上角100x100像素的区域  
-    screenshot = pyautogui.screenshot(region=(0, 0, 100, 100))  
-  
-# 保存截图到文件  
-    screenshot.save('screenshot.png')
-    rectangle_detection.detect_and_move_to_rectangle("screenshot.png")  
+   
+
+
+    rectangle_detection.detect_and_move_to_rectangle("image2.jpg")  
+
   
 # 主程序入口  
 if __name__ == "__main__":  
